188a2870cf694adda29a466d09f6997d6442d465
VIINNAY CHANDEL <38590400+vinaychandel1@users.noreply.github.com>
2020-05-19 00:00:53 +0530

    Add files via upload

---
+++ 8. Scope/8. scope.js
@@ -0,0 +1,11 @@
+var dogName="Rugby";
+var printdogname=function(){
+    document.write(dogName);
+}
+var PrintCatName=function(){
+    var catName="Whiskers";
+    document.write(catName);
+}
+
+printdogname();
+PrintCatName();
