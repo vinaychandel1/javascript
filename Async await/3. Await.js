e7281846651556fa39c923b4e3773bc1089d837f
VIINNAY CHANDEL <38590400+vinaychandel1@users.noreply.github.com>
2020-06-10 23:30:58 +0530

    Update 3. Await.js

--- Async await/3. Await.js
+++ Async await/3. Await.js
@@ -19,4 +19,24 @@ f();
 /*
 
 The function execution “pauses” at the line (*) and resumes when the promise settles, 
-with result becoming its result. So the code above shows “done!” in one second.*/
+with result becoming its result. So the code above shows “done!” in one second.
+
+Let’s emphasize: await literally makes JavaScript wait until the promise settles, and then go on with the result. That doesn’t cost any CPU resources, because the engine can do other jobs in the meantime: execute other scripts, handle events, etc.
+
+It’s just a more elegant syntax of getting the promise result than promise.then, easier to read and write.
+
+
+
+Can’t use await in regular functions
+If we try to use await in non-async function, there would be a syntax error:
+
+
+
+
+function f() {
+  let promise = Promise.resolve(1);
+  let result = await promise; // Syntax error
+}
+We will get this error if we do not put async before a function. As said, await only works inside an async function.
+
+*/
